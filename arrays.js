885fa763a69b048b0c9cbb4953cbaeb7e4d2250e
Jordan Brasko <jbrasko0508@gmail.com>
2020-10-17 23:45:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,6 @@ var ingredient6 = "garlic"
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray() {
-  var first = '1'
+  var first = ['1']
   first = ['foo', ...first]
 }
