d15ddc970b8b533a8c30ce367dbb67e919b6935e
Jordan Brasko <jbrasko0508@gmail.com>
2020-10-18 01:02:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,5 +33,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array;
 }
 function removeElementFromBeginningOfArray (array) {
-  var array = 
+  array = array.slice(1)
+  return array
 }
