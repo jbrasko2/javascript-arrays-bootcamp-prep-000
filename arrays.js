8949f657dadb76222eb25148d7f09195fb36712b
Jordan Brasko <jbrasko0508@gmail.com>
2020-10-18 00:16:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,5 +10,5 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element) {
   var newArray = array;
   newArray.unshift(element);
-  return 
+  return newArray
 }
